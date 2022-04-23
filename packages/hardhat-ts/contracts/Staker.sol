891b80d3967384e1d1ceaf5db6c12e887311e81f
gareth-was-here <gareth@garethveale.dev>
2022-04-23 18:47:04 +0200

    execute function

--- packages/hardhat-ts/contracts/Staker.sol
+++ packages/hardhat-ts/contracts/Staker.sol
@@ -6,12 +6,11 @@ import './ExampleExternalContract.sol';
 
 contract Staker {
   ExampleExternalContract public exampleExternalContract;
-
   mapping(address => uint256) public balances;
-
   event Stake(address, uint256);
-
   uint256 public constant threshold = 1 ether;
+  uint256 public deadline = block.timestamp + 30 seconds;
+  bool openForWithdraw;
 
   constructor(address exampleExternalContractAddress) public {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
@@ -25,6 +24,16 @@ contract Staker {
     console.log('received %s from %s, new balance: %s', msg.value, msg.sender, balances[msg.sender]);
 
     console.log('contract balance: %s', address(this).balance);
+  }
+
+  function execute() {
+    require(block.timestamp > deadline, 'deadline has not passed');
+
+    if (address(this).balance < threshold) {
+      openForWithdraw = true;
+    } else {
+      exampleExternalContract.complete{value: address(this).balance}();
+    }
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
