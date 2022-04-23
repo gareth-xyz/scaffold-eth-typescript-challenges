012e6dd314a5b7c7e2a18af726a0f8d106f1255e
gareth-was-here <gareth@garethveale.dev>
2022-04-23 19:35:04 +0200

    time left and withdraw functions

--- packages/hardhat-ts/contracts/Staker.sol
+++ packages/hardhat-ts/contracts/Staker.sol
@@ -26,14 +26,29 @@ contract Staker {
     console.log('contract balance: %s', address(this).balance);
   }
 
-  function execute() {
-    require(block.timestamp > deadline, 'deadline has not passed');
+  function execute() public {
+    require(timeLeft() == 0, 'deadline has not passed');
 
     if (address(this).balance < threshold) {
+      console.log('execute called but balance < threshold');
       openForWithdraw = true;
     } else {
       exampleExternalContract.complete{value: address(this).balance}();
     }
+  }
+
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    }
+    return deadline - block.timestamp;
+  }
+
+  function withdraw() external {
+    require(openForWithdraw = true, 'contract is not open for withdraw');
+    require(balances[msg.sender] > 0, 'caller has no funds deposited');
+    payable(msg.sender).transfer(balances[msg.sender]);
+    balances[msg.sender] = 0;
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
